01fd9ed5063aa4c13bf4b42a3742d7c5bbb4067d
LastUrsa <DRDohr@gmail.com>
2026-05-23 19:51:29 -0400

    Refresh generated release artifacts metadata

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,5 +1,83 @@
 export namespace app {
 	
+	export class TwitchSettings {
+	    clientId: string;
+	    channelId: string;
+	    channelName: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new TwitchSettings(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.clientId = source["clientId"];
+	        this.channelId = source["channelId"];
+	        this.channelName = source["channelName"];
+	    }
+	}
+	export class OBSSettings {
+	    host: string;
+	    port: number;
+	    allowRemote: boolean;
+	    passwordConfigured: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new OBSSettings(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.host = source["host"];
+	        this.port = source["port"];
+	        this.allowRemote = source["allowRemote"];
+	        this.passwordConfigured = source["passwordConfigured"];
+	    }
+	}
+	export class Settings {
+	    obs: OBSSettings;
+	    sources: config.SourcesConfig;
+	    sceneMappings: config.SceneMapping[];
+	    twitch: TwitchSettings;
+	    modeProfiles: config.ModeProfile[];
+	    startupMode: string;
+	    currentMode: string;
+	    refreshRewardsOnStartup: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new Settings(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.obs = this.convertValues(source["obs"], OBSSettings);
+	        this.sources = this.convertValues(source["sources"], config.SourcesConfig);
+	        this.sceneMappings = this.convertValues(source["sceneMappings"], config.SceneMapping);
+	        this.twitch = this.convertValues(source["twitch"], TwitchSettings);
+	        this.modeProfiles = this.convertValues(source["modeProfiles"], config.ModeProfile);
+	        this.startupMode = source["startupMode"];
+	        this.currentMode = source["currentMode"];
+	        this.refreshRewardsOnStartup = source["refreshRewardsOnStartup"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class Status {
 	    config: Settings;
 	    currentMode: string;
@@ -141,6 +219,42 @@ export namespace app {
 	
 	
 	
+	
+	export class SettingsInput {
+	    config: Settings;
+	    obsPassword: string;
+	    updateObsPassword: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new SettingsInput(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.config = this.convertValues(source["config"], Settings);
+	        this.obsPassword = source["obsPassword"];
+	        this.updateObsPassword = source["updateObsPassword"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
 	export class TwitchReward {
 	    id: string;
 	    title: string;
@@ -160,119 +274,6 @@ export namespace app {
 	        this.is3DOnly = source["is3DOnly"];
 	        this.manageable = source["manageable"];
 	    }
-	}
-
-	export class OBSSettings {
-	    host: string;
-	    port: number;
-	    allowRemote: boolean;
-	    passwordConfigured: boolean;
-
-	    static createFrom(source: any = {}) {
-	        return new OBSSettings(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.host = source["host"];
-	        this.port = source["port"];
-	        this.allowRemote = source["allowRemote"];
-	        this.passwordConfigured = source["passwordConfigured"];
-	    }
-	}
-	export class TwitchSettings {
-	    clientId: string;
-	    channelId: string;
-	    channelName: string;
-
-	    static createFrom(source: any = {}) {
-	        return new TwitchSettings(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.clientId = source["clientId"];
-	        this.channelId = source["channelId"];
-	        this.channelName = source["channelName"];
-	    }
-	}
-	export class Settings {
-	    obs: OBSSettings;
-	    sources: config.SourcesConfig;
-	    sceneMappings: config.SceneMapping[];
-	    twitch: TwitchSettings;
-	    modeProfiles: config.ModeProfile[];
-	    startupMode: string;
-	    currentMode: string;
-	    refreshRewardsOnStartup: boolean;
-
-	    static createFrom(source: any = {}) {
-	        return new Settings(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.obs = this.convertValues(source["obs"], OBSSettings);
-	        this.sources = this.convertValues(source["sources"], config.SourcesConfig);
-	        this.sceneMappings = this.convertValues(source["sceneMappings"], config.SceneMapping);
-	        this.twitch = this.convertValues(source["twitch"], TwitchSettings);
-	        this.modeProfiles = this.convertValues(source["modeProfiles"], config.ModeProfile);
-	        this.startupMode = source["startupMode"];
-	        this.currentMode = source["currentMode"];
-	        this.refreshRewardsOnStartup = source["refreshRewardsOnStartup"];
-	    }
-
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
-	}
-	export class SettingsInput {
-	    config: Settings;
-	    obsPassword: string;
-	    updateObsPassword: boolean;
-
-	    static createFrom(source: any = {}) {
-	        return new SettingsInput(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.config = this.convertValues(source["config"], Settings);
-	        this.obsPassword = source["obsPassword"];
-	        this.updateObsPassword = source["updateObsPassword"];
-	    }
-
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
 	}
 
 }
@@ -297,46 +298,6 @@ export namespace config {
 	        this.vtuberVisible = source["vtuberVisible"];
 	        this.pngTuberVisible = source["pngTuberVisible"];
 	        this.enable3DRewards = source["enable3DRewards"];
-	    }
-	}
-	export class RewardMapping {
-	    rewardId: string;
-	    rewardName: string;
-	    is3DOnly: boolean;
-	    manageable: boolean;
-	
-	    static createFrom(source: any = {}) {
-	        return new RewardMapping(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.rewardId = source["rewardId"];
-	        this.rewardName = source["rewardName"];
-	        this.is3DOnly = source["is3DOnly"];
-	        this.manageable = source["manageable"];
-	    }
-	}
-	export class TwitchConfig {
-	    clientId: string;
-	    channelId: string;
-	    channelName: string;
-	    accessToken: string;
-	    refreshToken: string;
-	    tokenExpiry: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new TwitchConfig(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.clientId = source["clientId"];
-	        this.channelId = source["channelId"];
-	        this.channelName = source["channelName"];
-	        this.accessToken = source["accessToken"];
-	        this.refreshToken = source["refreshToken"];
-	        this.tokenExpiry = source["tokenExpiry"];
 	    }
 	}
 	export class SceneMapping {
@@ -381,73 +342,6 @@ export namespace config {
 	        this.pngTuberItemId = source["pngTuberItemId"];
 	    }
 	}
-	export class OBSConfig {
-	    host: string;
-	    port: number;
-	    password: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new OBSConfig(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.host = source["host"];
-	        this.port = source["port"];
-	        this.password = source["password"];
-	    }
-	}
-	export class Config {
-	    obs: OBSConfig;
-	    sources: SourcesConfig;
-	    sceneMappings: SceneMapping[];
-	    twitch: TwitchConfig;
-	    rewardMappings: RewardMapping[];
-	    modeProfiles: ModeProfile[];
-	    startupMode: string;
-	    currentMode: string;
-	    refreshRewardsOnStartup: boolean;
-	
-	    static createFrom(source: any = {}) {
-	        return new Config(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.obs = this.convertValues(source["obs"], OBSConfig);
-	        this.sources = this.convertValues(source["sources"], SourcesConfig);
-	        this.sceneMappings = this.convertValues(source["sceneMappings"], SceneMapping);
-	        this.twitch = this.convertValues(source["twitch"], TwitchConfig);
-	        this.rewardMappings = this.convertValues(source["rewardMappings"], RewardMapping);
-	        this.modeProfiles = this.convertValues(source["modeProfiles"], ModeProfile);
-	        this.startupMode = source["startupMode"];
-	        this.currentMode = source["currentMode"];
-	        this.refreshRewardsOnStartup = source["refreshRewardsOnStartup"];
-	    }
-	
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
-	}
-	
-	
-	
-	
-	
 
 }
 
